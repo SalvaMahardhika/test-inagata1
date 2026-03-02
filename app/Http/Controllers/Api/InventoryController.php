eef6265f18b8f7b990cedea553df172383d44816
SalvaMahardhika <yoi198808@gmail.com>
2026-03-02 17:00:37 +0700

    buat inventory controller

--- app/Http/Controllers/Api/InventoryController.php
+++ app/Http/Controllers/Api/InventoryController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Product;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class InventoryController extends Controller
 {
-    //
+    // GET /inventory/value
+    public function totalValue()
+    {
+        $totalValue = Product::selectRaw('SUM(price * stock_quantity) as total_inventory_value')
+            ->value('total_inventory_value');
+
+        return response()->json([
+            'total_inventory_value' => $totalValue ?? 0
+        ], 200);
+    }
 }
